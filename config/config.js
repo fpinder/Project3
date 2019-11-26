816d5d1a5304de78d92540ff9791e4df635ab0f9
Frank Pinder <fpinder@comcast.net>
2019-11-25 20:40:13 -0800

    update config file for creadit card

--- config/config.js
+++ config/config.js
@@ -8,8 +8,8 @@ const config = {
     (process.env.MONGO_PORT || '27017') +
     '/mernproject',
   stripe_connect_test_client_id: 'YOUR_stripe_connect_test_client',
-  stripe_test_secret_key: 'YOUR_stripe_test_secret_key',
-  stripe_test_api_key: 'YOUR_stripe_test_api_key'
+  stripe_test_secret_key: 'sk_test_nfJr02wEaLyYiVh1H9WVEDdR00c1blqxXO',
+  stripe_test_api_key: 'pk_test_e5MHUjVsY571cQDL9f9Num1n00APakqzwO'
 }
 
 export default config
